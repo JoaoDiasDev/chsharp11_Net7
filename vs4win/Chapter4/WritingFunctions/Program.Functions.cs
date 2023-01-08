ca55433cc08d34e10301d9a73e6ceb3d11d0cc3d
JoaoDias <joaodiasworking@gmail.com>
2023-01-08 12:52:29 -0300

    End at page 214

--- vs4win/Chapter4/WritingFunctions/Program.Functions.cs
+++ vs4win/Chapter4/WritingFunctions/Program.Functions.cs
@@ -50,6 +50,11 @@
         return amount * rate;
     }
 
+    /// <summary>
+    /// Pass a 32-bit integer and it will be converted into its ordinal equivalent.
+    /// </summary>
+    /// <param name="number">Number as a cardinal value e.g. 1, 2, 3, and so on.</param>
+    /// <returns>Number as an ordinal value e.g. 1st, 2nd, 3rd, and so on.</returns>
     static string CardinalToOrdinal(int number)
     {
         int lastTwoDigits = number % 100;
@@ -80,6 +85,92 @@
         }
         WriteLine();
     }
+    static int Factorial(int number)
+    {
+        if (number < 0)
+        {
+            throw new ArgumentException(message:
+            $"The factorial function is defined for non-negative integers only.Input: {number}",
+        paramName: nameof(number));
+        }
+        else if (number == 0)
+        {
+            return 1;
+        }
+        else
+        {
+            checked
+            {
+                return number * Factorial(number - 1);
+            }
+        }
+    }
+
+    static void RunFactorial()
+    {
+        for (int i = -2; i <= 15; i++)
+        {
+            try
+            {
+                WriteLine($"{i}! = {Factorial(i):N0}");
+            }
+            catch (OverflowException)
+            {
+                WriteLine($"{i}! is too big for a 32-bit integer.");
+            }
+            catch (Exception ex)
+            {
+                WriteLine($"{i}! throws {ex.GetType()}: {ex.Message}");
+
+            }
+        }
+    }
+
+    static int FibImperative(int term)
+    {
+        if (term == 1)
+        {
+            return 0;
+        }
+        else if (term == 2)
+        {
+            return 1;
+        }
+        else
+        {
+            return FibImperative(term - 1) + FibImperative(term - 2);
+        }
+    }
+
+    static void RunFibImperative()
+    {
+        for (int i = 1; i <= 30; i++)
+        {
+            WriteLine("The {0} term of the Fibonacci sequence is {1:N0}.",
+            arg0: CardinalToOrdinal(i),
+            arg1: FibImperative(term: i));
+        }
+    }
+
+    static int FibFunctional(int term) =>
+ term switch
+ {
+     1 => 0,
+     2 => 1,
+     _ => FibFunctional(term - 1) + FibFunctional(term - 2)
+ };
+
+    static void RunFibFunctional()
+    {
+        for (int i = 1; i <= 30; i++)
+        {
+            WriteLine("The {0} term of the Fibonacci sequence is {1:N0}.",
+            arg0: CardinalToOrdinal(i),
+            arg1: FibFunctional(term: i));
+        }
+    }
+
+
 }
 
 
