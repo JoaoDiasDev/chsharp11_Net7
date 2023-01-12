d82e903d54ce2ee7e45a8649278bbc37dbf62c84
JoaoDias <joaodiasworking@gmail.com>
2023-01-12 12:02:39 -0300

    Stopped at page 272 from the book

--- vs4win/Chapter5/PeopleApp/Program.cs
+++ vs4win/Chapter5/PeopleApp/Program.cs
@@ -55,3 +55,74 @@ gerrierAccount.Balance = 98;
 WriteLine(format: "{0} earned {1:C} interest.",
  arg0: gerrierAccount.AccountName,
  arg1: gerrierAccount.Balance * BankAccount.InterestRate);
+
+WriteLine($"{bob.Name} is a {Person.Species}");
+WriteLine($"{bob.Name} was born on {bob.HomePlanet}");
+
+Person blankPerson = new();
+WriteLine(format:
+ "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}.",
+ arg0: blankPerson.Name,
+ arg1: blankPerson.HomePlanet,
+ arg2: blankPerson.Instantiated);
+
+Person gunny = new(initialName: "Gunny", homePlanet: "Mars");
+WriteLine(format:
+ "{0} of {1} was created at {2:hh:mm:ss} on a {2:dddd}.",
+ arg0: gunny.Name,
+ arg1: gunny.HomePlanet,
+ arg2: gunny.Instantiated);
+
+bob.WriteToConsole();
+WriteLine(bob.GetOrigin());
+
+(string, int) fruit = bob.GetFruit();
+WriteLine($"{fruit.Item1}, {fruit.Item2} there are.");
+
+var fruitNamed = bob.GetNamedFruit();
+WriteLine($"There are {fruitNamed.Number} {fruitNamed.Name}.");
+
+var thing1 = ("Neville", 4);
+WriteLine($"{thing1.Item1} has {thing1.Item2} children.");
+var thing2 = (bob.Name, bob.Children.Count);
+WriteLine($"{thing2.Name} has {thing2.Count} children.");
+
+(string fruitName, int fruitNumber) = bob.GetFruit();
+WriteLine($"Deconstructed: {fruitName}, {fruitNumber}");
+
+// Deconstructing a Person
+var (name1, dob1) = bob; // implicitly calls the Deconstruct method
+WriteLine($"Deconstructed: {name1}, {dob1}");
+var (name2, dob2, fav2) = bob;
+WriteLine($"Deconstructed: {name2}, {dob2}, {fav2}");
+
+WriteLine(bob.SayHello());
+WriteLine(bob.SayHello("Emily"));
+
+WriteLine(bob.OptionalParameters());
+WriteLine(bob.OptionalParameters("Jump!", 98.5));
+WriteLine(bob.OptionalParameters(number: 52.7, command: "Hide!"));
+WriteLine(bob.OptionalParameters("Poke!", active: false));
+
+int a = 10;
+int b = 20;
+int c = 30;
+WriteLine($"Before: a = {a}, b = {b}, c = {c}");
+bob.PassingParameters(a, ref b, out c);
+WriteLine($"After: a = {a}, b = {b}, c = {c}");
+
+int d = 10;
+int e = 20;
+WriteLine($"Before: d = {d}, e = {e}, f doesn't exist yet!");
+// simplified C# 7.0 or later syntax for the out parameter
+bob.PassingParameters(d, ref e, out int f);
+WriteLine($"After: d = {d}, e = {e}, f = {f}");
+
+Person sam = new()
+{
+    Name = "Sam",
+    DateOfBirth = new(1969, 6, 25)
+};
+WriteLine(sam.Origin);
+WriteLine(sam.Greeting);
+WriteLine(sam.Age);
