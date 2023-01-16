01f6e7054951936edeb1eb43bebbb7b27fd13585
JoaoDias <joaodiasworking@gmail.com>
2023-01-16 14:31:56 -0300

    Stopped at page 292 from the book.

--- vs4win/Chapter5/PeopleApp/Program.cs
+++ vs4win/Chapter5/PeopleApp/Program.cs
@@ -240,13 +240,50 @@ foreach (Passenger passenger in passengers)
 {
     decimal flightCost = passenger switch
     {
-        FirstClassPassenger p when p.AirMiles > 35000 => 1500M,
-        FirstClassPassenger p when p.AirMiles > 15000 => 1750M,
-        FirstClassPassenger _ => 2000M,
-        BusinessClassPassenger _ => 1000M,
+        // C# 8 syntax
+        //FirstClassPassenger p when p.AirMiles > 35000 => 1500M,
+        //FirstClassPassenger p when p.AirMiles > 15000 => 1750M,
+        //FirstClassPassenger _ => 2000M,
+        //BusinessClassPassenger _ => 1000M,
+        //CoachClassPassenger p when p.CarryOnKG < 10.0 => 500M,
+        //CoachClassPassenger _ => 650M,
+        //_ => 800M
+
+        // C# 9 or later syntax
+        FirstClassPassenger p => p.AirMiles switch
+        {
+            > 35000 => 1500M,
+            > 15000 => 1750M,
+            _ => 2000M
+        },
+        BusinessClassPassenger => 1000M,
         CoachClassPassenger p when p.CarryOnKG < 10.0 => 500M,
-        CoachClassPassenger _ => 650M,
+        CoachClassPassenger => 650M,
         _ => 800M
     };
+
     WriteLine($"Flight costs {flightCost:C} for {passenger}");
 }
+
+ImmutablePerson jeff = new()
+{
+    FirstName = "Jeff",
+    LastName = "Winger"
+};
+//jeff.FirstName = "Geoff";
+
+ImmutableVehicle car = new()
+{
+    Brand = "Mazda MX-5 RF",
+    Color = "Soul Red Crystal Metallic",
+    Wheels = 4
+};
+ImmutableVehicle repaintedCar = car
+ with
+{ Color = "Polymetal Grey Metallic" };
+WriteLine($"Original car color was {car.Color}.");
+WriteLine($"New car color is {repaintedCar.Color}.");
+
+ImmutableAnimal oscar = new("Oscar", "Labrador");
+var (who, what) = oscar; // calls Deconstruct method
+WriteLine($"{who} is a {what}.");
