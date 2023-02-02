00bbd69d1a31324817d9cda69ca03f98c13162b3
JoaoDias <joaodiasworking@gmail.com>
2023-02-02 11:21:06 -0300

    Stopped at page 319 from the book.

---
+++ vs4win/Chapter6/PacktLibrary/DvdPlayer.cs
@@ -0,0 +1,16 @@
+﻿namespace Packt.Shared;
+public class DvdPlayer : IPlayable
+{
+    public void Pause()
+    {
+        WriteLine("DVD player is pausing.");
+    }
+    public void Play()
+    {
+        WriteLine("DVD player is playing.");
+    }
+    public void Stop()
+    {
+        WriteLine("Overriding Stop implementation");
+    }
+}
