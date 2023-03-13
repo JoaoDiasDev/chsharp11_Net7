a2c13e216df7b37a8aee88462007477dcfc9b856
JoaoDias <joaodiasworking@gmail.com>
2023-03-12 23:14:13 -0300

    Commiting some changes to workingWithText

--- vs4win/Chapter8/WorkingWithText/Program.cs
+++ vs4win/Chapter8/WorkingWithText/Program.cs
@@ -24,3 +24,12 @@ bool startsWithM = company.StartsWith("M");
 bool containsN = company.Contains("N");
 WriteLine($"Text: {company}");
 WriteLine($"Starts with M: {startsWithM}, contains an N: {containsN}");
+
+string recombined = string.Join(" => ", citiesArray);
+WriteLine(recombined);
+string fruit = "Apples";
+decimal price = 0.39M;
+DateTime when = DateTime.Today;
+WriteLine($"Interpolated: {fruit} cost {price:C} on {when:dddd}.");
+WriteLine(string.Format("string.Format: {0} cost {1:C} on {2:dddd}.",
+ arg0: fruit, arg1: price, arg2: when));
